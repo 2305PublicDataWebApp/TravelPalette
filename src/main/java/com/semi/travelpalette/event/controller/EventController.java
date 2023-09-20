f54149c4dc0ee1eb2ad2b220edace53f8fc24585
JeonSollll <jeonsol1996@gmail.com>
2023-09-20 16:08:20 +0900

    [FEAT] 23/09/20 16:07
    1. 이벤트 게시판 삭제 수정

--- src/main/java/com/semi/travelpalette/event/controller/EventController.java
+++ src/main/java/com/semi/travelpalette/event/controller/EventController.java
@@ -154,11 +154,11 @@ public class EventController {
 		try {
 			int result = eService.deleteByNo(eventNo);
 			if(result > 0) {
-				mv.addObject("msg", "여행정보가 삭제되었습니다");
+				mv.addObject("msg", "이벤트가 삭제되었습니다");
 				mv.addObject("url", "/event/list.tp");
 				mv.setViewName("common/successPage");
 			} else {
-				mv.addObject("msg", "[서비스실패] 삭제가 완료되지 않았습니다.");
+				mv.addObject("msg", "[서비스실패] 이벤트 삭제가 완료되지 않았습니다.");
 				mv.addObject("url", "/event/detail.tp?eventNo="+eventNo);
 				mv.setViewName("common/errorPage");
 			}
